139793dbe1d1951e0b3c23421cc8077f46d8ed4f
Gaël <g.bertrand@it-students.fr>
2021-04-09 23:25:10 +0200

    :sparkles: adding ToggleQuestion.jsx

--- src/Components/ToggleQuestion.jsx
+++ src/Components/ToggleQuestion.jsx
@@ -1,12 +1,35 @@
-public function ToggleQuestion ({objectQuestion}) {
-    return
-        <div>
-            <h2>{objectQuestion.question}</h2>
-            <input type="checkbox" />
-            <label htmlFor={objectQuestion.answer.name}></label>
-        </div>
-    )
-}
+import React, {Fragment} from 'react';
+import Switch from '@material-ui/core/Switch';
 
+
+
+function ToggleQuestion ({objectQuestion})  {
+
+    const [state, setState] = React.useState({
+        checkedA: false,
+    });
+    const handleChange = (event) => {
+        setState({ ...state, [event.target.name]: event.target.checked });
+    };
+
+    return(
+        <Fragment>
+            <h2>
+                <label className="switch" >
+                    Vrai/Faux? : {objectQuestion.question}
+                </label>
+            </h2>
+            <input  type="hidden" value={state.checkedA} name={objectQuestion.answer.name}/>
+            <br/>
+            <Switch
+                checked={state.checkedA}
+                onChange={handleChange}
+                color="primary"
+                name="checkedA"
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+            />
+        </Fragment>
+    );
+}
 
 export default ToggleQuestion
